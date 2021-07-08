b97ad38ce663b52d82b9dbdd1414cd4c917db3e5
Raymond Wanyoike <raymond.wanyoike@gmail.com>
2021-07-08 20:23:28 +0300

    update

--- dot_zsh/20.goenv.plugin.zsh
+++ dot_zsh/20.goenv.plugin.zsh
@@ -1,6 +1,6 @@
 # Ref: https://github.com/syndbg/goenv
 
-export GOENV_GOPATH_PREFIX="${HOME}/SRC/golang"
+export GOENV_GOPATH_PREFIX="${XDG_DATA_HOME}/go"
 export PATH="${HOME}/.goenv/bin:${PATH}"
 if command -v goenv &> /dev/null; then
   eval "$(goenv init -)"
