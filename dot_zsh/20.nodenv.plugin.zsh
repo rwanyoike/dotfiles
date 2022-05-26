2dee382a94da912c2cca23967b216bf7a4104880
Raymond Wanyoike <raymond.wanyoike@gmail.com>
2022-05-26 16:12:07 +0300

    update

--- dot_zsh/20.nodenv.plugin.zsh
+++
@@ -1,6 +0,0 @@
-# Ref: https://github.com/nodenv/nodenv
-
-export PATH="${HOME}/.nodenv/bin:${PATH}"
-if command -v nodenv &> /dev/null; then
-  eval "$(nodenv init -)"
-fi
--- dot_zsh/20.rbenv.plugin.zsh
+++
@@ -1,6 +0,0 @@
-# Ref: https://github.com/rbenv/rbenv
-
-export PATH="${HOME}/.rbenv/bin:${PATH}"
-if command -v rbenv &> /dev/null; then
-  eval "$(rbenv init -)"
-fi
