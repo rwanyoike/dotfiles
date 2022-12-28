a9fd8781e1a215767148e792117d3d47cf625cbe
Raymond Wanyoike <raymond.wanyoike@gmail.com>
2022-12-28 14:06:40 +0300

    Update dotfiles

--- dot_zsh/20.qt5.plugin.zsh
+++
@@ -1,6 +0,0 @@
-# Ref: https://wiki.archlinux.org/index.php/qt
-#      https://wiki.archlinux.org/index.php/Wayland
-
-export QT_QPA_PLATFORM=wayland-egl
-export QT_QPA_PLATFORMTHEME=gnome
-export QT_STYLE_OVERRIDE=kvantum-dark
