788d5aa7a6700bd963b5f496a9502693138eece5
Raymond Wanyoike <raymond.wanyoike@gmail.com>
2024-07-29 20:10:28 +0300

    update

--- dot_local/bin/executable_sway-session.sh
+++ dot_local/bin/executable_sway-session.sh
@@ -8,25 +8,22 @@ export XDG_SESSION_TYPE=wayland
 
 SESSION_TARGET=sway-session.target
 SESSION_SHUTDOWN_TARGET=sway-session-shutdown.target
-VARIABLES="DISPLAY SWAYSOCK WAYLAND_DISPLAY XDG_CURRENT_DESKTOP XDG_SESSION_DESKTOP XDG_SESSION_TYPE"
+VARIABLES=(DISPLAY SWAYSOCK WAYLAND_DISPLAY XDG_CURRENT_DESKTOP XDG_SESSION_DESKTOP XDG_SESSION_TYPE)
 
-if systemctl --user -q is-active ${SESSION_TARGET}; then
+if systemctl --user -q is-active "${SESSION_TARGET}"; then
   echo "Another Sway session found; refusing to overwrite the variables"
   exit 1
 fi
 
-# shellcheck disable=SC2086
-dbus-update-activation-environment --systemd ${VARIABLES}
+dbus-update-activation-environment --systemd "${VARIABLES[@]}"
 
 systemctl --user reset-failed
-# shellcheck disable=SC2086
-systemctl --user import-environment ${VARIABLES}
-systemctl --user start ${SESSION_TARGET}
+systemctl --user import-environment "${VARIABLES[@]}"
+systemctl --user start "${SESSION_TARGET}"
 
 function session_cleanup {
-  systemctl --user start --job-mode=replace-irreversibly ${SESSION_SHUTDOWN_TARGET}
-  # shellcheck disable=SC2086
-  systemctl --user unset-environment ${VARIABLES}
+  systemctl --user start --job-mode=replace-irreversibly "${SESSION_SHUTDOWN_TARGET}"
+  systemctl --user unset-environment "${VARIABLES[@]}"
 }
 
 trap session_cleanup INT TERM
